c45307972fc533d7bc0a72602248e218e4923d28
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-08-31 16:23:32 +0200

    oupsi no productajoutés en vu

--- front/js/product.js
+++ front/js/product.js
@@ -16,7 +16,12 @@ fetch(`http://localhost:3000/api/products/${productId}`) // on fetch du coup l'u
         elementDesProduits (product); // le beau produit est là ! on console log et bim les infos du produit, puis qu'a les dispatch aux bons endroits
         console.log(product);  // ça fonctionne, et ça fait du bien ! 
     })
-        
+
+    .catch(function(error) {
+        let itemerror = document.querySelector(".item");
+        itemerror.innerHTML = "Pas de Kanap pour toi ! ";
+        itemerror.style.textAlign = "center";
+    })
 
      let elementDesProduits = function(product) {  // les elem des produits on les dispatch
             document.querySelector(".item__img").innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
@@ -27,23 +32,49 @@ fetch(`http://localhost:3000/api/products/${productId}`) // on fetch du coup l'u
             // meme combat avec le prix et son id 
             document.getElementById("description").innerText += `${product.description}`;
             // tu l'as vu venir ? meme chose encore une fois !
-            
-            // les couleurs toujours pas avec getbyid et query vu qu'il y en a plusieurs doit y avoir un truc à faire, à voir !
             document.querySelector('title').innerText = `${product.name}`;
+            // Le title aussi ! 
 
-            for(let color of product.colors){
-            let i = 0; i < product.colors.length; i++;
-            document.getElementById("colors").innerText += `${product.colors}`;
-            }
+
+// les couleurs toujours pas avec getbyid et query vu qu'il y en a plusieurs doit y avoir un truc à faire, à voir !
+           let pRainbow = document.getElementById("colors"); // on dit que #colors c'est p 
+           for (let i =0; i < product.colors.length; i++) { // on boucle pour ajouter les couleurs tant qu'il y en a
+            let option = document.createElement("option"); // on dit que option c'est créer l'élément option
+            let colors = i; // on dit que colors c'est i ! 
+            option.innerText = product.colors[colors]; // pb j'ai toutes les couleurs parce que c'est un variable, idiot fallait mettre entre crochet pour un array !!!!!!
+            pRainbow.appendChild(option); // pour finir on appendchild a p pour qu'option ajoute un noeud en dessous ! 
+            // merci d'avoir lu mon tedtalk !
+           }
       }
 
-   
+      let ajouterPanier = function() {
+        
+        const button = document.querySelector("#addToCart");
+        const quantityKanap = document.querySelector("#quantity"); // On def quelques constantes pour les récupérer 
+        const colorKanap = document.querySelector("#colors");
 
+        button.addEventListener("click", function(cliquetis) { // on écoute sur le button le click
 
+            cliquetis.preventDefault(); // on fait notre action et pas celle par defaut
+            
+            if (quantityKanap.value > 0 && quantityKanap.value < 100) { // si on a entre 0 et 100 KANAP
 
-
-
-
-
-
-
+                
+                
+                let productAjoutés = {
+                    _id: productId,
+                    quantity:(document.querySelector("#quantity").value), 
+                    color: colorKanap.value,
+                    
+            };  // On recup les trois valeurs id quantité et couleur demandé dasn étapes clés !
+            
+            console.log();(productAjoutés);
+               // au début javais fait un log sur chaque click sur la page, mais maintenant que je veux cibler #addToCart j'ai R ! 
+                
+        }
+                else{
+                    alert("oupsi doupsi")
+                }
+    })
+    
+    }
