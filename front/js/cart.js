36dc37ad28dc5d7c41936bf05de843719af5aa72
Vicente  Dyl <95346160+DyVIcente@users.noreply.github.com>
2022-09-07 11:40:17 +0200

    page scriptjs

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,78 +1,83 @@
-let products = JSON.parse(localStorage.getItem("cart"));
-console.log(products);
 
+let productsLS = JSON.parse(localStorage.getItem("prod"));
+console.log(productsLS);
+
+main();
+function main(){
+    displayCart();
+}
 
 function displayCart() {
 
-    if (products === null || produitLocalStorage == 0) {  // si products est null ou égale à 0 
+    if (productsLS === null || productsLS == 0) {  // si products est null ou égale à 0 
         console.log("cart vide");
     } else {
-      //  console.log("cart products présent");
+        //  console.log("cart products présent");
 
-    for (let prod in products) { // Boucler sur les produits du panier
+        for (let prod in productsLS) { // Boucler sur les produits du panier
 
-        let produitArticle = document.createElement("article");
-        document.querySelector("#cart__items").appendChild(produitArticle);   //article  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
-        produitArticle.classList.add("cart__item");                            // chercher pour ajouter dataid et datacolor
+            let produitArticle = document.createElement("article");
+            document.querySelector("#cart__items").appendChild(produitArticle);   //article  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+            produitArticle.classList.add("cart__item");                            // chercher pour ajouter dataid et datacolor
 
-        let divImgage = document.createElement("div");      // div <div class="cart__item__img">
-        produitArticle.appendChild(divImgage);
-        divImgage.classList.add("cart__item__img");
-
-
-        let image = document.createElement("img");
-        divImgage.appendChild(image);
-        image.src = products[prod].product.imageUrl;    //<img src="../images/product01.jpg" alt="Photographie d'un canapé">
-        image.alt = products[prod].product.altTxt;
+            let divImgage = document.createElement("div");      // div <div class="cart__item__img">
+            produitArticle.appendChild(divImgage);
+            divImgage.classList.add("cart__item__img");
 
 
-        let carItemContent = document.createElement("div");   //<div class="cart__item__content">
-        produitArticle.appendChild(carItemContent);
-        carItemContent.classList.add("cart__item__content");
+            let image = document.createElement("img");
+            divImgage.appendChild(image);
+            image.src = productsLS[prod].product.imageUrl;    //<img src="../images/product01.jpg" alt="Photographie d'un canapé">
+            image.alt = productsLS[prod].product.altTxt;
 
-        let carItemContentDesp = document.createElement("div"); // <div class="cart__item__content__description">
-        carItemContent.appendChild(carItemContentDesp);
-        carItemContentDesp.classList.add("cart__item__content__description");
 
-        let itemTitle = document.createElement("h2");   // <h2>Nom du produit</h2>
-        carItemContentDesp.appendChild(itemTitle);
-        itemTitle.innerHTML = products[prod].product.name;
+            let carItemContent = document.createElement("div");   //<div class="cart__item__content">
+            produitArticle.appendChild(carItemContent);
+            carItemContent.classList.add("cart__item__content");
 
-        let itemColor = document.createElement("p"); // <p>Vert</p>
-        itemTitle.appendChild(itemColor);
-        itemColor.innerHTML = products[prod].product.color;
+            let carItemContentDesp = document.createElement("div"); // <div class="cart__item__content__description">
+            carItemContent.appendChild(carItemContentDesp);
+            carItemContentDesp.classList.add("cart__item__content__description");
 
-        let itemPrice = document.createElement("p"); //  <p>42,00 €</p>
-        itemTitle.appendChild(itemPrice);
-        itemPrice.innerHTML = products[prod].product.price;
+            let itemTitle = document.createElement("h2");   // <h2>Nom du produit</h2>
+            carItemContentDesp.appendChild(itemTitle);
+            itemTitle.innerHTML = productsLS[prod].product.name;
 
-        let cartItemSetting = document.createElement("div"); //<div class="cart__item__content__settings">
-        carItemContent.appendChild(cartItemSetting);
-        cartItemSetting.classList.add("cart__item__content__settings");
+            let itemColor = document.createElement("p"); // <p>Vert</p>
+            itemTitle.appendChild(itemColor);
+            itemColor.innerHTML = productsLS[prod].product.color;
 
-        let cartItemSettingquantity = document.createElement("div"); //<div class="cart__item__content__settings__quantity">
-        cartItemSetting.appendChild(cartItemSettingquantity);
-        cartItemSettingDelete.classList.add("cart__item__content__settings__quantity");
+            let itemPrice = document.createElement("p"); //  <p>42,00 €</p>
+            itemTitle.appendChild(itemPrice);
+            itemPrice.innerHTML = productsLS[prod].product.price;
 
-        let itemQuantity = document.createElement("p"); // <p>Qté : </p>
-        cartItemSettingquantity.appendChild(itemQuantity);
-        // itemQuantity.innerHTML = ?
+            let cartItemSetting = document.createElement("div"); //<div class="cart__item__content__settings">
+            carItemContent.appendChild(cartItemSetting);
+            cartItemSetting.classList.add("cart__item__content__settings");
 
-        let itemQuantityInput = document.createElement("input"); // <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-        cartItemSettingquantity.appendChild(itemQuantityInput);
-        itemQuantityInput.classList.add("itemQuantity");      // ajouter les type name etc;;
+            let cartItemSettingquantity = document.createElement("div"); //<div class="cart__item__content__settings__quantity">
+            cartItemSetting.appendChild(cartItemSettingquantity);
+            cartItemSettingDelete.classList.add("cart__item__content__settings__quantity");
 
-        let itemContentSettingDelete = document.createElement("div"); // <div class="cart__item__content__settings__delete">
-        cartItemSetting.appendChild(itemContentSettingDelete);
-        itemContentSettingDelete.classList.add("cart__item__content__settings__delete");
+            let itemQuantity = document.createElement("p"); // <p>Qté : </p>
+            cartItemSettingquantity.appendChild(itemQuantity);
+            // itemQuantity.innerHTML = ?
 
-        let supprimer = document.createElement("p"); //<p class="deleteItem">Supprimer</p>
-        itemContentSettingDelete.appendChild(supprimer);
-        supprimer.classList.add("deleteItem");
+            let itemQuantityInput = document.createElement("input"); // <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+            cartItemSettingquantity.appendChild(itemQuantityInput);
+            itemQuantityInput.classList.add("itemQuantity");      // ajouter les type name etc;;
+
+            let itemContentSettingDelete = document.createElement("div"); // <div class="cart__item__content__settings__delete">
+            cartItemSetting.appendChild(itemContentSettingDelete);
+            itemContentSettingDelete.classList.add("cart__item__content__settings__delete");
+
+            let supprimer = document.createElement("p"); //<p class="deleteItem">Supprimer</p>
+            itemContentSettingDelete.appendChild(supprimer);
+            supprimer.classList.add("deleteItem");
+
+        }
 
     }
-
-      }
 
 
 }
--- front/js/script.js
+++ front/js/script.js
@@ -1,39 +1,57 @@
+
+main();
+
+function main(){
+  getArticles();
+}
+
 // allo l'API tu m'entends ? si oui, envois les infos en json stp
-fetch("http://localhost:3000/api/products/")
-  .then(function (response) { 
-    if (response.ok) {
-      return response.json();
-    }
-  }) 
-  // Création d'une liste des produits à partir des données de l'API
-  .then(function(products) {
+function getArticles() {
+  fetch("http://localhost:3000/api/products/")
+
+    .then(function (response) {  
+        return response.json();     
+    })
+
     
-// Pour les produits on change le html pour incorporer ce qu'on nous donne dans index
-    for(let product of products) {
-      let i = 0; i < product.length; i++; 
-      // boucle for qui démare à 0 et qui continue d'etre executer jusqu'a qu'il n'y ai plus de produit, et ça c'est beau
-      // On chope l'id items et on change son HTML , bim c'est  magique ! 
-      document.getElementById("items").innerHTML += 
+// On affiche un message d'erreur des plus délicieux
+    .catch(function (error) {
+      let itemsNotFound = document.querySelector(".items");
+      itemsNotFound.innerHTML = "Erreur, vérifier que le port 3000 est ouvert, merci, prennez soin de vous, on se voit à la piscine, Bisous";
 
-      `<a href="./product.html?id=${product._id}">
-                                                     
-      <article>
-                                                       
-      <img src="${product.imageUrl}" alt="${product.altTxt}">
-                                                       
-      <h3 class="productName">${product.name}</h3>
-                                                       
-      <p class="productDescription">${product.description}</p>
-                                                      
-      </article>
-                                                   
-      </a>`
-    }
-  })
+    })
 
-  // On affiche un message d'erreur des plus délicieux
-  .catch(function(error) {
-    let itemsNotFound = document.querySelector(".items");
-    itemsNotFound.innerHTML = "Erreur, vérifier que le port 3000 est ouvert, merci, prennez soin de vous, on se voit à la piscine, Bisous";
+    //  On ajoute les éléments des kanap 
+    .then(function (resultatAPI) {
+      const articles = resultatAPI;  // on dit qu'article  est le resustat de l'api 
+      console.table(articles); // on affiche un tableau dans la console avec toutes les infos qu'on a recup
+      // 
+      for (let article in articles) {
+        // du coup pour article dans les resultats de l'api on ajoute les elements 
+        let linkProduit = document.createElement("a");
+        document.querySelector(".items").appendChild(linkProduit);
+        linkProduit.href = `product.html?id=${resultatAPI[article]._id}`;
 
-  });
+        let articleProduit = document.createElement("article");
+        linkProduit.appendChild(articleProduit);
+
+        let imageProduit = document.createElement("img");
+        articleProduit.appendChild(imageProduit);
+        imageProduit.src = resultatAPI[article].imageUrl;
+        imageProduit.alt = resultatAPI[article].altTxt;
+
+        let nameProduit = document.createElement("h3");
+        articleProduit.appendChild(nameProduit);
+        nameProduit.classList.add("productName");
+        nameProduit.innerHTML = resultatAPI[article].name;
+
+        let descriptionProduit = document.createElement("p");
+        articleProduit.appendChild(descriptionProduit);
+        descriptionProduit.classList.add("productDescription");
+        descriptionProduit.innerHTML = resultatAPI[article].description;
+
+      }
+    });
+}
+
+  
